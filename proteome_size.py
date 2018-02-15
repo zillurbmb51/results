14603e71539d541cbd87bbd6dbca204c116c51b8
zillurbmb51 <zillurbmb51@gmail.com>
2018-02-15 00:34:51 -0400

    Update proteome_size.py

--- proteome_size.py
+++ proteome_size.py
@@ -1,8 +1,9 @@
-from Bio.SeqIO.FastaIO import SimpleFastaParser
-count=0
+#This is from biopython tutorial not modified
+from Bio.SeqIO.FastaIO import SimpleFastaParser #Load fasta parser from biopython
+count=0 #Start counting as 0
 total=0
-with open(raw_input("Enter the fasta file:"),'rU') as fasta:
- for t,s in SimpleFastaParser(fasta):
+with open(raw_input("Enter the fasta file:"),'rU') as fasta:       #Load the fasta file
+ for t,s in SimpleFastaParser(fasta): #parse sequence and update within loop
   count+=1
   total+=len(s)
-print("%i records with proteome size %i" %(count,total))
+print("%i records with proteome size %i" %(count,total)) #Print the number of sequences and AAs
